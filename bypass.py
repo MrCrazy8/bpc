d19308c76dea217116406405e09c6a1f4f57edff
MrCrazy8 <49766205+MrCrazy8@users.noreply.github.com>
2019-04-19 03:00:22 +0430

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -1,13 +1,11 @@
 import os , pyfiglet , time
 
-os.system("clear")
-os.system("clear")
+os.system("clear")                                                                      os.system("clear")
 os.system("clear")
 
 class color :
 
-          YELL = '\033[93m '
-          GREEN = '\033[92m '
+          YELL = '\033[93m '                                                                      GREEN = '\033[92m '
           BLUE ='\033[94m '
           END = '\033[0m'
 
@@ -31,8 +29,62 @@ $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 ''' + color.END)
 print ("  ")
 
-y = input(color.GREEN + "Enter The Url >>>" + color.END)
+print (color.BLUE + '''
+
+[1] Test With mail
+
+[2] Test With ftp
+
+[3] Test With ssh
+
+[4] Test With cpanel
+
+[5] Test With whm
+
+[99] Exit
+
+''' + color.END)
 
 print ("   ")
 
-os.system("ping "+"ftp."+y)
+u = int(input(color.YELL + "Enter The Number ~# " + color.END))
+
+print ("   ")
+if u == 1:
+
+    y = input(color.GREEN + "Enter The Url >>>" + color.END)
+
+    print ("   ")
+
+    os.system("ping "+"mail."+y)
+if u == 2:
+
+    q = input(color.GREEN + "Enter The Url >>>" + color.END)
+
+    print ("   ")
+
+    os.system("ping "+"ftp."+q)
+if u == 3:
+
+    w = input(color.GREEN + "Enter The Url >>>" + color.END)
+
+    print ("   ")
+
+    os.system("ping "+"ssh."+w)
+if u == 4:
+
+    i = input(color.GREEN + "Enter The Url >>>" + color.END)
+
+    print ("   ")
+
+    os.system("ping "+"cpanel."+i)
+if u == 5:
+
+    t = input(color.GREEN + "Enter The Url >>>" + color.END)
+
+    print ("   ")
+
+    os.system("ping "+"whm."+t)
+if u == 99:
+
+    os.system("clear")
